50b7daebfb0579fd5d19ae44f3fd1f30221c9164
Ahmad Tahani <ahmad.tahani@gmail.com>
2016-03-14 14:37:36 +0330

    add launcher icon and remove client information

--- app/src/main/java/com/atahani/retrofit_sample/utility/ClientConfigs.java
+++ app/src/main/java/com/atahani/retrofit_sample/utility/ClientConfigs.java
@@ -4,5 +4,6 @@ package com.atahani.retrofit_sample.utility;
  * contain client information such as BASE_URL
  */
 public class ClientConfigs {
-    public static final String REST_API_BASE_URL = "http://172.20.10.2:8081/api/v1/";
+    //TODO: should get network ip address like 192.168.1.2 and replace inside REST_API_BASE_URL
+    public static final String REST_API_BASE_URL = "http://IP_ADDRESS:8081/api/v1/";
 }
